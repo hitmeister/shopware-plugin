00b3fadcd3e9c47603012a8b7a0d33fa6417f467
Maksim Naumov <maksim.naumov@hitmeister.de>
2016-01-21 08:35:23 +0100

    DEV-2228: Add some logs to the Hm controller

--- Controllers/Frontend/Hm.php
+++ Controllers/Frontend/Hm.php
@@ -1,5 +1,6 @@
 <?php
 
+use Psr\Log\LoggerInterface;
 use Hitmeister\Component\Api\Transfers\Constants;
 use ShopwarePlugins\HitmeMarketplace\Components\Exporter;
 use ShopwarePlugins\HitmeMarketplace\Components\Ordering;
@@ -34,12 +35,12 @@ class Shopware_Controllers_Frontend_Hm extends Enlight_Controller_Action
     protected function confirmSubscription()
     {
         if ('subscribe' !== $this->Request()->get('mode') || !$this->Request()->has('challenge')) {
-            $this->Response()->setHttpResponseCode(400);
+            $this->processResponse(400, 'Unexpected mode or empty challenge string');
             return;
         }
 
         // Reply on challenge
-        $this->Response()->setBody($this->Request()->get('challenge'));
+        $this->processResponse(200, $this->Request()->get('challenge'));
     }
 
     /**
@@ -50,10 +51,18 @@ class Shopware_Controllers_Frontend_Hm extends Enlight_Controller_Action
         $resource = $this->Request()->getParam('resource');
         $eventName = $this->Request()->getParam('event_name');
 
+        if (empty($resource) || empty($eventName)) {
+            $this->processResponse(400, 'Resource or event name is empty');
+            return;
+        }
+
+        /** @var LoggerInterface $logger */
+        $logger = Shopware()->Container()->get('pluginlogger');
+
         switch ($eventName) {
             case Constants::EVENT_NAME_ORDER_NEW:
                 if (!preg_match('~/orders/(.*)/~', $resource, $matches)) {
-                    $this->Response()->setHttpResponseCode(400);
+                    $this->processResponse(400, sprintf('Order ID not found in "%s"', $resource));
                     return;
                 }
                 $resourceId = $matches[1];
@@ -61,7 +70,8 @@ class Shopware_Controllers_Frontend_Hm extends Enlight_Controller_Action
                 try {
                     $this->getOrderService()->process($resourceId);
                 } catch (Exception $e) {
-                    $this->Response()->setHttpResponseCode(500);
+                    $logger->error('Error on `order_new` event processing', ['exception' => $e]);
+                    $this->processResponse(500, $e->getMessage());
                 }
                 break;
 
@@ -70,7 +80,7 @@ class Shopware_Controllers_Frontend_Hm extends Enlight_Controller_Action
 
             case Constants::EVENT_NAME_ORDER_UNIT_STATUS_CHANGED:
                 if (!preg_match('~/order-units/(.*)/~', $resource, $matches)) {
-                    $this->Response()->setHttpResponseCode(400);
+                    $this->processResponse(400, sprintf('Order unit ID not found in "%s"', $resource));
                     return;
                 }
                 $resourceId = $matches[1];
@@ -78,11 +88,26 @@ class Shopware_Controllers_Frontend_Hm extends Enlight_Controller_Action
                 try {
                     $this->getOrderService()->cancelOrderUnit($resourceId);
                 } catch (Exception $e) {
-                    $this->Response()->setHttpResponseCode(500);
+                    $logger->error('Error on `order_unit_status_changed` event processing', ['exception' => $e]);
+                    $this->processResponse(500, $e->getMessage());
                 }
 
                 break;
+
+            default:
+                $this->processResponse(400, sprintf('Unsupported event "%s"', $eventName));
+                break;
         }
+    }
+
+    /**
+     * @param int $code
+     * @param string $body
+     */
+    private function processResponse($code, $body)
+    {
+        $this->Response()->setHttpResponseCode($code);
+        $this->Response()->setBody($body);
     }
 
     public function exportAction()
