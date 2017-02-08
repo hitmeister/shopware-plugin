b7ee7cefdc3b9d60662056b1b832d88d34589d16
Mario Bone-Winkel <m.bone-winkel@mediahaus.de>
2017-02-08 08:08:55 +0100

    HPS-7 - Relabeling

--- Bootstrap/Form.php
+++ Bootstrap/Form.php
@@ -29,18 +29,18 @@ class Form
      */
     public function create()
     {
-        $this->form->setDescription('<p>Real ist eines der größten deutschen Online-Shopping-Portale mitten im Herzen von Köln. 100% sicheres, einfaches, günstiges und persönliches Einkaufs- und Verkaufserlebnis. Die Zahlungsabwicklung und auch sämtliche Marketingmaßnahmen werden von Hitmeister übernommen. Angebote werden anhand der EAN eingestellt, die Abrechnung erfolgt anhand eines einfachen Gebührenmodells. Bei Fragen steht Ihnen die Händlerbetreuung telefonisch unter <b>+49-221-975979-79</b> oder per E-Mail an <b>partnermanagement@real.de</b> gerne zur Verfügung.</p><p>Um zu starten, bitten wir Sie die unten abgefragten Informationen zu hinterlegen, damit die Abwicklung zwischen Ihrem System und Hitmeister reibungslos funktioniert.  Einige der Informationen finden Sie in Ihrem Real-Versandpartner Account unter Shopeinstellungen, daher bitten wir Sie, sich parallel in Ihrem Real-Account einzuloggen.</p>');
+        $this->form->setDescription('<p>real.de ist ein offener Marktplatz, der Händlern in über 5.000 Kategorien einen attraktiven Vertriebskanal für über 30 unterschiedliche Versandländer bietet. Die Internetpräsenz der real,- SB-Warenhaus GmbH vereint eine hohe Markenbekanntheit mit gezieltem Multi-Channel-Marketing und bietet Ihnen eine optimale Reichweitenstärke für Ihre Angebote.</p><br /><p>real.de - Attraktiv für Händler:</p><ul><li>Markenbekanntheit durch 285 SB-Warenhäuser und 23 Millionen Handzettel pro Woche</li><li>Komfortables Einstellen anhand der EAN/GTIN</li><li>Kostenfreie Bewerbung Ihrer Artikel in Preissuchmaschinen, SEO und SEA</li><li>Direkter und persönlicher Service</li><li>Verkaufserlöse direkt von real.de</li><li>Keine Vertragslaufzeiten</li><li>Transparentes Kostenmodell</li></ul><p>real.de - Attraktiv für Endkunden:</p><ul><li>Sammeln und Einlösen von Payback-Punkten</li><li>Über 10 verschiedene Bezahlmethoden</li> <li>0%-Finanzierung: Von Zuhause mittels Online-Ident-Verfahren</li></ul>');
 
         $this->form->setElement('button', 'openForm', array(
             'label' => 'Jetzt registrieren!',
-            'handler' => 'function () { window.open("https://www.hitmeister.de/versandpartner/online-marktplatz/"); }',
+            'handler' => 'function () { window.open("https://www.real.de/versandpartner/software-schnittstellen/"); }',
         ));
 
         // Api settings
         $this->form->setElement('text', 'clientKey', array(
             'itemId' => 'hmClientKey',
             'label' => 'API: Client key',
-            'description' => 'Diese Information finden Sie im Hitmeister Account unter Shopseinstellungen; API.',
+            'description' => 'Diese Information finden Sie im real.de Account unter Shopseinstellungen; API.',
             'required' => true,
             'scope' => \Shopware\Models\Config\Element::SCOPE_SHOP,
         ));
@@ -48,7 +48,7 @@ class Form
         $this->form->setElement('text', 'secretKey', array(
             'itemId' => 'hmSecretKey',
             'label' => 'API: Secret key',
-            'description' => 'Diese Information finden Sie im Hitmeister-Account unter Shopseinstellungen; API.',
+            'description' => 'Diese Information finden Sie im real.de Account unter Shopseinstellungen; API.',
             'required' => true,
             'afterSubTpl' => '<h2>Ablauf Plugininstallation</h2>
             <ul>
@@ -75,7 +75,7 @@ class Form
         // Stock management
         $this->form->setElement('select', 'syncStatus', array(
           'label' => 'Stock: Sync status',
-          'description' => 'Bitte legen Sie den globalen Sync Status für den Subshop fest. ACHTUNG: Wenn sie Artikel eines Shops bei Hitmeister blocken oder löschen wollen, dann müssen Sie den entsprechenden Aufruf vorher im Hitmeister Modul starten.',
+          'description' => 'Bitte legen Sie den globalen Sync Status für den Subshop fest. ACHTUNG: Wenn sie Artikel eines Shops bei real.de blocken oder löschen wollen, dann müssen Sie den entsprechenden Aufruf vorher im real.de Modul starten.',
           'value' => 0,
           'required' => true,
           'store' => array(
@@ -106,7 +106,7 @@ class Form
 
         $this->form->setElement('select', 'defaultCondition', array(
             'label' => 'Stock: Default article condition',
-            'description' => 'Bitte legen Sie den globalen Artikelzustand fest. Diese Einstellung wird für alle auf Hitmeister angebotenen Artikel übernommen.',
+            'description' => 'Bitte legen Sie den globalen Artikelzustand fest. Diese Einstellung wird für alle auf real.de angebotenen Artikel übernommen.',
             'value' => Constants::CONDITION_NEW,
             'required' => true,
             'store' => array(
@@ -123,7 +123,7 @@ class Form
         $this->form->setElement('combo', 'defaultShippingGroup', array(
           'itemId' => 'defaultShippingGroup',
           'label' => 'Shipping Group: Default shipping code',
-          'description' => 'Bitte legen Sie die globale Shipping Group fest. Diese Einstellung wird für alle auf Hitmeister angebotenen Artikel übernommen.',
+          'description' => 'Bitte legen Sie die globale Shipping Group fest. Diese Einstellung wird für alle auf real.de angebotenen Artikel übernommen.',
           'value' => '',
           'valueField'=>'name',
           'displayField'=>'name',
@@ -196,7 +196,7 @@ class Form
         $paymentMethods = Shopware()->Db()->fetchAll('SELECT id, name FROM s_core_paymentmeans', array(), \PDO::FETCH_NUM);
         $this->form->setElement('select', 'defaultPaymentMethod', array(
             'label' => 'Orders: Default payment method',
-            'description' => 'Bitte wählen Sie eine Bezahlmethode, die Ihrem System mitteilt, dass der Kauf bereits bezahlt ist, da Hitmeister die Zahlungsabwicklung für Sie übernimmt.',
+            'description' => 'Bitte wählen Sie eine Bezahlmethode, die Ihrem System mitteilt, dass der Kauf bereits bezahlt ist, da real.de die Zahlungsabwicklung für Sie übernimmt.',
             'required' => true,
             'value' => !empty($paymentMethods) ? $paymentMethods[0][0] : '',
             'store' => $paymentMethods,
